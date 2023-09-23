c51fd7166b39a24ef785fc4b5b1f246ea06f7fe8
Lovealone1 <89592158+Lovealone1@users.noreply.github.com>
2023-09-22 21:03:08 -0500

    Feat: Creacion del proyecto y componente lista

---
+++ components/Lista.vue
@@ -0,0 +1,62 @@
+<template>
+    <section>
+        <div class="container-row">
+          <div id="liste" class="container-wrap" >
+            <Character v-for="character in characters" :key="character.id" :character="character"/>
+          </div>
+        </div>
+       
+      </section>
+    </template>
+    
+    <script>
+    import axios from 'axios'
+    
+    export default {
+      // eslint-disable-next-line vue/multi-word-component-names
+      name: 'Lista',
+    
+      components: {
+        Character
+      },
+    
+      data() {
+        return {
+          characters:[]
+        }
+      },
+    
+        beforeCreate() {
+          axios.get(`http://gateway.marvel.com/v1/public/characters?ts=1&apikey=5bd987f4470c9f45b2dae9f51d1387db&hash=7c42b00f9898de246920fa7b29236598`)
+            // eslint-disable-next-line no-console
+            .then((response) => {this.characters = response.data.data.results; console.log(response.data.data.results)})
+      },
+    
+      methods:{
+        
+          }
+    }
+    </script>
+    
+    <style>
+    .container-row {
+      display : flex;
+      flex-direction: row;
+      justify-content: space-around;
+    }
+    
+    .container-wrap {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+      align-items: flex-start;
+    }
+    
+    #liste{
+      width: 1050px;
+      height: auto;
+      margin: 40px;
+    }
+    
+    </style>
+    
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="personaje">
+    <List/>
+  </div>
+</template>
+
+
+<script>
+import List from '../components/Lista'
+
+export default {
+  name: 'Index',
+  components: {
+    List
+  }
+}
+</script>
+
+<style>
+#personaje {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  height: 150%;
+  margin-top: 270px;
+}
+</style>
