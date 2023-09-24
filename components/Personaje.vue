ada699cb2f77c7f31c8fdbba642f1fd0f69f1496
Lovealone1 <89592158+Lovealone1@users.noreply.github.com>
2023-09-24 18:30:56 -0500

    Feat: Creación del botón mostrar descripción

--- components/Personaje.vue
+++ components/Personaje.vue
@@ -1,153 +1,168 @@
-  <template>
-    <div>
-      <div id="lignes" class="container-col">
-        <div
-          id="image"
-          :style="{ backgroundImage: 'url(' + character.thumbnail.path + '.' + character.thumbnail.extension + ')' }"
-          @click="toggleCharacterData"
-        ></div>
-        <div id="nom" class="container-row">
-          <h1 @click="toggleCharacterData">{{ character.name }}</h1>
-        </div>
+<template>
+  <div>
+    <div id="lignes" class="container-col">
+      <div
+        id="image"
+        :style="{ backgroundImage: 'url(' + character.thumbnail.path + '.' + character.thumbnail.extension + ')' }"
+        @click="toggleCharacterData"
+      ></div>
+      <div id="nom" class="container-row">
+        <h1 @click="toggleCharacterData">{{ character.name }}</h1>
       </div>
-  
-      <v-card v-if="characterData" class="character-card">
-        <v-card-title>
-  
-        </v-card-title>
-  
+    </div>
+
+    <v-card v-if="characterData" class="character-card">
+      <v-card-title>
+
+      </v-card-title>
+      <v-card-actions class="d-flex align-center justify-center"> 
+        <v-btn color="grey" dark class="text-white" @click="dialog = true">Mostrar descripción</v-btn>
+      </v-card-actions>
+      <v-card-text>
+        <div>
+          <b>Comics:</b> {{ character.comics.available }},
+          <b>Series:</b> {{ character.series.available }}
+        </div>
+        <div>
+          <b>Historias:</b> {{ character.stories.available }},
+          <b>Eventos:</b> {{ character.events.available }}
+        </div>
+
+        <div class="info-row">
+          <b>Otras participaciones:</b>
+          <ul>
+            <li v-for="serie in characterSeries" :key="serie.name">{{ serie.name }}</li>
+          </ul>
+        </div>
+      </v-card-text>
+
+      <v-dialog v-model="dialog" max-width="500px">
+        <v-card>
+          <v-card-title class="headline">
+
+          </v-card-title>
         <v-card-text>
-          <div>
-            <b>Comics:</b> {{ character.comics.available }},
-            <b>Series:</b> {{ character.series.available }}
-          </div>
-          <div>
-            <b>Historias:</b> {{ character.stories.available }},
-            <b>Eventos:</b> {{ character.events.available }}
-          </div>
-  
-          <div class="info-row">
-            <b>Otras participaciones:</b>
-            <ul>
-              <li v-for="serie in characterSeries" :key="serie.name">{{ serie.name }}</li>
-            </ul>
-          </div>
+          <p>{{ character.description }}</p>
         </v-card-text>
       </v-card>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    // eslint-disable-next-line vue/multi-word-component-names
-    name: "Personaje",
-    props: {
-      character: {
-        type: Object,
-        required: true,
-      },
-    },
-    data() {
-      return {
-        characterData: false,
-      };
-    },
-    computed: {
-      characterImageStyle() {
-        return {
-          backgroundImage: `url(${this.character.thumbnail.path}.${this.character.thumbnail.extension})`,
-        };
-      },
-      characterSeries() {
-        return this.character.series.items.slice(0, 3);
-      },
-      containerColStyle() {
-      return {
-        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-      };
-    },
-    containerRowStyle() {
-      return {
-        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-      };
+    </v-dialog>
+      
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Personaje",
+  props: {
+    character: {
+      type: Object,
+      required: true,
     },
   },
-    methods: {
-      toggleCharacterData() {
-        this.characterData = !this.characterData;
-      },
+  data() {
+    return {
+      characterData: false,
+      dialog: false, // Renombramos a 'dialog'
+    };
+  },
+  computed: {
+    characterImageStyle() {
+      return {
+        backgroundImage: `url(${this.character.thumbnail.path}.${this.character.thumbnail.extension})`,
+      };
     },
-  };
-  </script>
-  
-  <style scoped>
-  .container-row {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  }
-  
-  .container-col {
-    display: flex;
-    flex-direction: column;
-    margin: 5px;
-  }
-  
-  #lignes {
-    width: 250px;
-    height: 300px;
-    border-radius: 0px;
-  }
-  
-  #lignes:hover {
-    border: 2px solid #525252;
-  }
-  
-  #image {
-    width: 100%;
-    height: 100%;
-    background-size: cover;
-    border-radius: 0px 0px 0px 0px;
-    border: 1px solid rgba(180, 180, 180, 0.781);
-  }
-  
-  #nom {
-    background-color: white;
-    border: 1px solid rgba(180, 180, 180, 0.781);
-    width: 100%;
-    height: 80px;
-    border-radius: 0px 0px 0px 0px;
-  }
-  
-  h1 {
-    font-size: 20px;
-    height: auto;
-    text-align: center;
-    color: black;
-  }
-  
-  h1:hover {
-    font-size: 25px;
-    color: #000000;
-  }
-  
-  .character-card {
-    max-width: 250px; /* Establece un ancho máximo */
-    margin: 20px auto; /* Centra la tarjeta y controla los márgenes */
-  }
-  
-  .info-row ul {
-    list-style: none;
-    padding: 0;
-  }
-  
-  .character-info {
-    padding: 16px; /* Ajusta el espacio interno de la v-card */
-  }
-  
-  .character-info div, .character-info ul {
-    margin-bottom: 8px; /* Ajusta el espaciado entre los elementos de texto */
-  }
-  </style>
+    characterSeries() {
+      return this.character.series.items.slice(0, 3);
+    },
+    containerColStyle() {
+    return {
+      boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+    };
+  },
+  containerRowStyle() {
+    return {
+      boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+    };
+  },
+},
+  methods: {
+    toggleCharacterData() {
+      this.characterData = !this.characterData;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container-row {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+.container-col {
+  display: flex;
+  flex-direction: column;
+  margin: 5px;
+}
+
+#lignes {
+  width: 250px;
+  height: 300px;
+  border-radius: 0px;
+}
+
+#lignes:hover {
+  border: 2px solid #525252;
+}
+
+#image {
+  width: 100%;
+  height: 100%;
+  background-size: cover;
+  border-radius: 0px 0px 0px 0px;
+  border: 1px solid rgba(180, 180, 180, 0.781);
+}
+
+#nom {
+  background-color: white;
+  border: 1px solid rgba(180, 180, 180, 0.781);
+  width: 100%;
+  height: 80px;
+  border-radius: 0px 0px 0px 0px;
+}
+
+h1 {
+  font-size: 20px;
+  height: auto;
+  text-align: center;
+  color: black;
+}
+
+h1:hover {
+  font-size: 25px;
+  color: #000000;
+}
+
+.character-card {
+  max-width: 250px; /* Establece un ancho máximo */
+  margin: 20px auto; /* Centra la tarjeta y controla los márgenes */
+}
+
+.info-row ul {
+  list-style: none;
+  padding: 0;
+}
+
+.character-info {
+  padding: 16px; /* Ajusta el espacio interno de la v-card */
+}
+
+.character-info div, .character-info ul {
+  margin-bottom: 8px; /* Ajusta el espaciado entre los elementos de texto */
+}
+</style>
